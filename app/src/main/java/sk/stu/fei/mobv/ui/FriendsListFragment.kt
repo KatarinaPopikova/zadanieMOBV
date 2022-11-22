77ec15c9f48e3936c94bf09dfb4b071317de21ab
kStasova <stasovakatka6688@gmail.com>
2022-11-22 18:31:22 +0100

    feat: create fragments

--- app/src/main/java/sk/stu/fei/mobv/ui/FriendsListFragment.kt
+++ app/src/main/java/sk/stu/fei/mobv/ui/FriendsListFragment.kt
@@ -1,10 +1,11 @@
-package sk.stu.fei.mobv
+package sk.stu.fei.mobv.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import sk.stu.fei.mobv.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Register.newInstance] factory method to
+ * Use the [FriendsListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Register : Fragment() {
+class FriendsListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class Register : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return inflater.inflate(R.layout.fragment_friends_list, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class Register : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Register.
+         * @return A new instance of fragment FriendsListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Register().apply {
+            FriendsListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
