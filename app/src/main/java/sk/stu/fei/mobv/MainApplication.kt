4566e945276da90f04d556b9bfd445a4a9939a89
kStasova <stasovakatka6688@gmail.com>
2022-11-28 15:08:58 +0100

    feat: add application class

---
+++ app/src/main/java/sk/stu/fei/mobv/MainApplication.kt
@@ -0,0 +1,12 @@
+package sk.stu.fei.mobv
+
+import android.app.Application
+import sk.stu.fei.mobv.database.AppDatabase
+import sk.stu.fei.mobv.network.RestApiService
+import sk.stu.fei.mobv.repository.Repository
+
+class MainApplication : Application(){
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
+    val restApiService: RestApiService by lazy { RestApiService.create(this) }
+    val repository by lazy { Repository.getInstance(restApiService, database.barDao()) }
+}
